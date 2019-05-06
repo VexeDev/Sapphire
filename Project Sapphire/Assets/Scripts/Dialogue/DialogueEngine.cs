dbe835806f6eb06e148a4252096a0d8666ddbaaf
VexeDev <44173192+VexeDev@users.noreply.github.com>
2019-05-06 15:01:34 -0400

    update for grim

---
+++ Project Sapphire/Assets/Scripts/Dialogue/DialogueEngine.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueEngine : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+    public Text nameText;
+    public Text messageText;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        anim.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.Name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence ()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        messageText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            messageText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue ()
+    {
+        anim.SetBool("IsOpen", false);
+    }
+}
---
+++ Project Sapphire/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    bool hasTriggered;
+    public GameObject dialogueEngine;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Submit") && hasTriggered == false)
+        {
+            hasTriggered = true;
+            TriggerDialogue();
+        } else if (Input.GetButtonDown("Submit") && hasTriggered == true)
+        {
+            dialogueEngine.GetComponent<DialogueEngine>().DisplayNextSentence();
+        }
+    }
+
+    public void TriggerDialogue ()
+    {
+        FindObjectOfType<DialogueEngine>().StartDialogue(dialogue);
+    }
+}
